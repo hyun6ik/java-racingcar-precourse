3b13dd438ef8c83f5e1d19ecc42220f5eee0caa2
hyun6ik <fkanwm@naver.com>
2022-04-23 20:45:06 +0900

    feat/message : 텍스트, 에러, 넘버 메시지 생성

---
+++ src/main/java/racingcar/nextstep/global/message/Number.java
@@ -0,0 +1,9 @@
+package racingcar.nextstep.global.message;
+
+public interface Number {
+
+    int RANDOM_MIN_VALUE = 1;
+    int RANDOM_MAX_VALUE = 9;
+    int ADVANCE_VALUE = 4;
+    int MIN_VALUE = 0;
+}
